74a077bab9f09b5b5d022d05a0b9e5ba760d929f
YurianKB <yuriankb@gmail.com>
2017-06-05 21:57:40 -0500

    Empezando funcionalidad

--- js/app.js
+++ js/app.js
@@ -17,30 +17,13 @@ var listaRestaurantes = [{
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 var cargarPagina = function() {
-  $("#get-location").click(obtenerUbicacion);
+  obtenerUbicacion();
+  crearElementos(restaurantes);
+  $("#formularioBusqueda").click(filtrarRestaurantes);
+  $('select').material_select();
 };
+
 
 var obtenerUbicacion = function(e) {
   if (navigator.geolocation) {
@@ -70,5 +53,68 @@ var mostrarMapa = function(coordenadas) {
     map: map
   });
 }
+
+
+
+var restaurantes = [{
+  "nombre": "Restaurante La Roma",
+  "tipoDeComida": "ComidaCorrida",
+  "direccion": "Mérida 243, C. U. Benito Juárez, 06700 Ciudad de México, CDMX",
+  "coordenadas": {
+    "lat": '19.4123409',
+    "lng": '-99.1580969',
+  }
+}, {
+  "nombre": "Helado Obscuro",
+  "tipoDeComida": "Helado",
+  "direccion": "Orizaba 203, Roma Nte., 06700 Ciudad de México, CDMX",
+  "coordenadas": {
+    "lat": '19.4141967',
+    "lng": '-99.1679291'
+  }
+
+}, {
+  "nombre": "Pizza del Perro Negro",
+  "tipoDeComida": "Pizza",
+  "direccion": "Parque España 3, Roma Nte., 06700 Ciudad de México, CDMX",
+  "coordenadas": {
+    "lat": '19.4182846',
+    "lng": '-99.167457'
+  }
+
+}];
+
+var crearElementos = function(restaurantes) {
+  var plantillaFinal = "";
+  restaurantes.forEach(function(restaurante) {
+    plantillaFinal += plantillaRestaurante.replace("__nombre__", restaurante.nombre)
+      .replace("__direccion__", restaurante.direccion)
+      .replace("__TipoComida__", restaurante.tipoDeComida)
+      .replace("__telefono__", restaurante.telefono)
+
+  });
+  $(".listadoDeComidas").html(plantillaFinal);
+};
+var filtrarRestaurantes = function(e) {
+  e.preventDefault();
+  var criterioBusqueda = $("input.select-dropdown").val().toLowerCase();
+  var palabraBusqueda = $("#foodFinder").val().toLowerCase();
+  var restaurantesFiltrados = restaurantes.filter(function(restaurante) {
+    if (criterioBusqueda === "tipo") {
+      return restaurante.tipoDeComida.toLowerCase().indexOf(palabraBusqueda) >= 0;
+
+    } else if (criterioBusqueda === "nombre") {
+      return restaurante.nombre.toLowerCase().indexOf(palabraBusqueda) >= 0;
+
+    } else if (criterioBusqueda === "direccion") {
+      return restaurante.direccion.toLowerCase().indexOf(palabraBusqueda) >= 0;
+
+    } else {
+      return "no te encuentras cerca de ese lugar ";
+    }
+  });
+  crearElementos(restaurantesFiltrados);
+};
+
 
 $(document).ready(cargarPagina);
